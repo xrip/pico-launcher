c3d4391eeb60d7cbd87f7cc7241dfd423825593a
xrip <xrip@xrip.ru>
2024-01-31 11:00:43 +0300

    tmp

--- drivers/vga-nextgen/vga.c
+++ drivers/vga-nextgen/vga.c
@@ -619,7 +619,7 @@ void graphics_init() {
     );
     //dma_channel_set_read_addr(dma_chan, &DMA_BUF_ADDR[0], false);
 
-    graphics_set_mode(VGA_320x240x256);
+    graphics_set_mode(TGA_320x200x16);
 
     irq_set_exclusive_handler(VGA_DMA_IRQ, dma_handler_VGA);
 
