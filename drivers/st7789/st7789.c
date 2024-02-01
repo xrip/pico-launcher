d56fac8a7a71a27515c66edbe2e355304a541d5a
xrip <xrip@xrip.ru>
2024-02-01 13:09:49 +0300

    TFT driver dma transfer and 16 bit pixel writing

--- drivers/st7789/st7789.c
+++ drivers/st7789/st7789.c
@@ -18,6 +18,7 @@
 
 #include "st7789.pio.h"
 #include "fnt6x8.h"
+#include "hardware/dma.h"
 
 #ifndef SCREEN_WIDTH
 #define SCREEN_WIDTH 320
@@ -38,6 +39,8 @@
 
 static uint sm = 0;
 static PIO pio = pio0;
+static uint st7789_chan;
+
 uint16_t __scratch_y("tft_palette") palette[256];
 
 uint8_t* text_buffer = NULL;
@@ -117,8 +120,36 @@ static inline void lcd_init(const uint8_t* init_seq) {
 
 static inline void start_pixels() {
     const uint8_t cmd = 0x2c; // RAMWR
+    st7789_lcd_wait_idle(pio, sm);
+    st7789_set_pixel_mode(pio, sm, false);
     lcd_write_cmd(&cmd, 1);
+    st7789_set_pixel_mode(pio, sm, true);
     lcd_set_dc_cs(1, 0);
+}
+
+void stop_pixels() {
+    st7789_lcd_wait_idle(pio, sm);
+    lcd_set_dc_cs(1, 1);
+    st7789_set_pixel_mode(pio, sm, false);
+}
+
+void create_dma_channel() {
+    st7789_chan = dma_claim_unused_channel(true);
+
+    dma_channel_config c = dma_channel_get_default_config(st7789_chan);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_16);
+    channel_config_set_dreq(&c, pio_get_dreq(pio, sm, true));
+    channel_config_set_read_increment(&c, true);
+    channel_config_set_write_increment(&c, false);
+
+    dma_channel_configure(
+        st7789_chan, // Channel to be configured
+        &c, // The configuration we just created
+        &pio->txf[sm], // The write address
+        NULL, // The initial read address - set later
+        0, // Number of transfers - set later
+        false // Don't start yet
+    );
 }
 
 void graphics_init() {
@@ -139,10 +170,13 @@ void graphics_init() {
     gpio_put(TFT_RST_PIN, 1);
     lcd_init(init_seq);
     gpio_put(TFT_LED_PIN, 1);
-    for (int i = 0; i < sizeof palette; i++ ) {
+
+    for (int i = 0; i < sizeof palette; i++) {
         graphics_set_palette(i, 0x0000);
     }
     clrScr(0);
+
+    create_dma_channel();
 }
 
 void inline graphics_set_mode(const enum graphics_mode_t mode) {
@@ -173,9 +207,19 @@ void clrScr(const uint8_t color) {
     uint32_t i = SCREEN_WIDTH * SCREEN_HEIGHT;
     start_pixels();
     while (--i) {
-        st7789_lcd_put16(pio, sm, 0x0000);
+        st7789_lcd_put_pixel(pio, sm, 0x0000);
     }
-    st7789_lcd_wait_idle(pio, sm);
+    stop_pixels();
+}
+
+void st7789_dma_pixels(const uint16_t* pixels, const uint num_pixels) {
+    // Ensure any previous transfer is finished.
+    dma_channel_wait_for_finish_blocking(st7789_chan);
+
+    dma_channel_hw_addr(st7789_chan)->read_addr = (uintptr_t)pixels;
+    dma_channel_hw_addr(st7789_chan)->transfer_count = num_pixels;
+    const uint ctrl = dma_channel_hw_addr(st7789_chan)->ctrl_trig;
+    dma_channel_hw_addr(st7789_chan)->ctrl_trig = ctrl | DMA_CH0_CTRL_TRIG_INCR_READ_BITS;
 }
 
 void __inline __scratch_y("refresh_lcd") refresh_lcd() {
@@ -185,7 +229,7 @@ void __inline __scratch_y("refresh_lcd") refresh_lcd() {
             start_pixels();
             for (int y = 0; y < SCREEN_HEIGHT; y++) {
                 // TODO add auto adjustable padding?
-                st7789_lcd_put16(pio, sm, 0x0000);
+                st7789_lcd_put_pixel(pio, sm, 0x0000);
 
                 for (int x = 0; x < TEXTMODE_COLS; x++) {
                     const uint16_t offset = (y / 8) * (TEXTMODE_COLS * 2) + x * 2;
@@ -194,23 +238,26 @@ void __inline __scratch_y("refresh_lcd") refresh_lcd() {
                     const uint8_t glyph_row = fnt6x8[c * 8 + y % 8];
 
                     for (uint8_t bit = 0; bit < 6; bit++) {
-                        st7789_lcd_put16(pio, sm, textmode_palette[(c && CHECK_BIT(glyph_row, bit))
+                        st7789_lcd_put_pixel(pio, sm, textmode_palette[(c && CHECK_BIT(glyph_row, bit))
                                                                        ? colorIndex & 0x0F
                                                                        : colorIndex >> 4 & 0x0F]);
                     }
                 }
-                st7789_lcd_put16(pio, sm, 0x0000);
+                st7789_lcd_put_pixel(pio, sm, 0x0000);
             }
+            stop_pixels();
             break;
         case GRAPHICSMODE_DEFAULT: {
             const uint8_t* bitmap = graphics_buffer;
             lcd_set_window(graphics_buffer_shift_x, graphics_buffer_shift_y, graphics_buffer_width,
-                       graphics_buffer_height);
+                           graphics_buffer_height);
             uint32_t i = graphics_buffer_width * graphics_buffer_height;
             start_pixels();
+            // st7789_dma_pixels(graphics_buffer, i);
             while (--i) {
-                st7789_lcd_put16(pio, sm, palette[*bitmap++]);
+               st7789_lcd_put_pixel(pio, sm, palette[*bitmap++]);
             }
+            stop_pixels();
         }
     }
 
@@ -219,9 +266,6 @@ void __inline __scratch_y("refresh_lcd") refresh_lcd() {
 
 
 void graphics_set_palette(const uint8_t i, const uint32_t color) {
-    palette[i] = (uint16_t) color;
+    palette[i] = (uint16_t)color;
 }
 
-void logMsg(char* msg) {
-    // dummy
-}
